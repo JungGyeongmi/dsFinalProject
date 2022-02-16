d1096f17bcad1feb550cde402ae0103ab02cd48f
JungGyeongmi <gm950715@gmail.com>
2022-02-16 21:23:56 +0900

    20220216/project created
    프로젝트 초기 생성

---
+++ phoncnic/src/main/java/com/ds/phoncnic/PhoncnicApplication.java
@@ -0,0 +1,13 @@
+package com.ds.phoncnic;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhoncnicApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhoncnicApplication.class, args);
+	}
+
+}
---
+++ phoncnic/src/test/java/com/ds/phoncnic/PhoncnicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ds.phoncnic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhoncnicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
