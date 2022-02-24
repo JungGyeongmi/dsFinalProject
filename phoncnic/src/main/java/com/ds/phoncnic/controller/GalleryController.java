56c21a7975896b54a426a630d60ea16a3d0ee48e
NagyeongHa <90600892+NagyeongHa@users.noreply.github.com>
2022-02-24 21:24:53 +0900

    chore: 컨트롤러

--- phoncnic/src/main/java/com/ds/phoncnic/controller/GalleryController.java
+++ phoncnic/src/main/java/com/ds/phoncnic/controller/GalleryController.java
@@ -3,7 +3,6 @@ package com.ds.phoncnic.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import lombok.extern.log4j.Log4j2;
@@ -12,6 +11,12 @@ import lombok.extern.log4j.Log4j2;
 @RequestMapping("/gallery")
 @Log4j2
 public class GalleryController {
+
+    //전시회 관람 페이지
+    @GetMapping("/list")
+    public String list(Model model){
+        log.info("gallery list........................");
+        return "gallery/list";
+    }
     
-   
 }
