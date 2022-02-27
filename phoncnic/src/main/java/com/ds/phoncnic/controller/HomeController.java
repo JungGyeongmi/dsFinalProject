d963711429b8cf82307c8996d40cb736c1eaa800
JungGyeongmi <gm950715@gmail.com>
2022-02-27 22:26:56 +0900

    chore : HomeController Log4j2 어노테이션 추가

--- phoncnic/src/main/java/com/ds/phoncnic/controller/HomeController.java
+++ phoncnic/src/main/java/com/ds/phoncnic/controller/HomeController.java
@@ -4,7 +4,10 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import lombok.extern.log4j.Log4j2;
+
 @Controller
+@Log4j2
 public class HomeController {
     
     @GetMapping({"","/"})
